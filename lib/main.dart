79143fb5d1eb4983c87a9a295e617c501246bfd3
SREEYUKTHA P <105417951+SREEYUKTHA-SNL@users.noreply.github.com>
2023-12-19 10:49:47 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_app/page/register.dart';
 import 'package:my_flutter_app/page/Loginpage.dart';
-import 'page/staff3.dart';
+import 'page/warden3.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: WardenPage3(),
     );
   }
 }

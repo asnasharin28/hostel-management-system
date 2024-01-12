c6a6b969a5bb1a5f635335970408855a91033d8a
SREEYUKTHA P <105417951+SREEYUKTHA-SNL@users.noreply.github.com>
2024-01-12 14:04:26 +0530

    scanner update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_app/Login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:my_flutter_app/page/codeQr.dart';
 
 import 'firebase_options.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login_Page(),
+      home: QrCodeScannerPage(),
       theme: ThemeData(scaffoldBackgroundColor: Color(0xFFFCF5ED)),
     );
   }
